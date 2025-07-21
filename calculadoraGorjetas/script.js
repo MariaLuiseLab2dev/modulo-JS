5c8b84b7dd84119aa29779199fbc7f7a00435f2e
MariaLuiseLab2dev <maria.luise@lab2dev.com>
2025-07-21 12:51:54 -0300

    Feature: Module JS 5: all exercises done.

---
+++ calculadoraGorjetas/script.js
@@ -0,0 +1,32 @@
+const inputValorConta = document.getElementById("valor-conta");
+const inputPorcentagemGorjeta = document.getElementById("porcentagem-gorjeta");
+const btnCalcular = document.getElementById("btn-calcular");
+const resultadoGorjeta = document.getElementById("resultado-gorjeta");
+const resultadoTotal = document.getElementById("resultado-total");
+
+const calcularGorjeta = () => {
+    const valorBruto = inputValorConta.value.trim().replace(",", ".");
+    const porcentagemBruta = inputPorcentagemGorjeta.value.trim().replace(",", ".");
+    
+    // Valida primeiro
+    const regexNumero = /^\d+(\.\d+)?$/;
+
+    if (!regexNumero.test(valorBruto) || !regexNumero.test(porcentagemBruta)) {
+        alert("Por favor, insira números válidos.");
+        return;
+    }
+
+    // Converte
+    const valorDaConta = parseFloat(valorBruto);
+    const valorDaporcentagem = parseFloat(porcentagemBruta);
+
+    // Faz a conta
+    const valorDaGorjeta = valorDaConta * (valorDaporcentagem / 100);
+    const valorTotal = valorDaConta + valorDaGorjeta;
+
+    // Expõe o resultado
+    resultadoGorjeta.textContent = `Gorjeta: R$ ${valorDaGorjeta.toFixed(2)}`;
+    resultadoTotal.textContent = `Total com gorjeta: R$ ${valorTotal.toFixed(2)}`;
+};
+
+btnCalcular.addEventListener('click', calcularGorjeta);
